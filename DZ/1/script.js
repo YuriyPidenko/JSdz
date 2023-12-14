a04aac3b0700aeab455ba223f2d382b14ef4e1a7
yuriy_pidenko <yuriy.pidenko@gmail.com>
2023-12-14 15:49:59 +0300

    first commit

---
+++ DZ/1/script.js
@@ -0,0 +1,8 @@
+let сelsius = 35;
+let fahrenheit = (9 / 5) * сelsius + 32;
+console.log(`Температура в Цельсиях: ${сelsius}
+Температура в Фаренгейтах: ${fahrenheit}`);
+
+let name = "Юра";
+let admin = name;
+console.log(admin);
